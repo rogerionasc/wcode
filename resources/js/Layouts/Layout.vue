cb457650220cfcbaff3497d7bb07406c0558eacb
Rogerio Nascimento <rogerionascimentosantos@gmail.com>
2024-03-13 06:09:07 -0300

    Layout base complete

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -313,43 +313,14 @@
                             <div class="col">
 
                                 <!-- Page pre-title -->
-                                <PageTitle/>
+                                <PageTitle :pageTitle="titleLayout"/>
 
-                                <div class="page-pretitle"> Overview</div>
+                                <div class="page-pretitle">{{  }}</div>
                             </div>
                             <!-- Page title actions -->
-                            <div class="col-auto ms-auto d-print-none">
-                                <div class="btn-list">
-                                    <span class="d-none d-sm-inline">
-                                        <a href="#" class="btn">
-                                            New view
-                                        </a>
-                                    </span>
-                                    <a href="#" class="btn btn-primary d-none d-sm-inline-block" data-bs-toggle="modal"
-                                       data-bs-target="#modal-report">
-                                        <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
-                                             viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                             stroke-linecap="round" stroke-linejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                            <path d="M12 5l0 14"/>
-                                            <path d="M5 12l14 0"/>
-                                        </svg>
-                                        Create new report
-                                    </a>
-                                    <a href="#" class="btn btn-primary d-sm-none btn-icon" data-bs-toggle="modal"
-                                       data-bs-target="#modal-report" aria-label="Create new report">
-                                        <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
-                                             viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                             stroke-linecap="round" stroke-linejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                            <path d="M12 5l0 14"/>
-                                            <path d="M5 12l14 0"/>
-                                        </svg>
-                                    </a>
-                                </div>
-                            </div>
+                            <slot name="ButtonCreate"/>
+
+
                         </div>
                     </div>
                 </div>
@@ -360,7 +331,7 @@
                             <div class="card-body">
 
                                 <!-- Page content -->
-
+                                <slot/>
 
                             </div>
                         </div>
@@ -520,12 +491,21 @@
 <script>
 
 import PageTitle from "@/Components/PageTitle.vue";
+import pageTitle from "@/Components/PageTitle.vue";
+import ButtonCreate from "@/Components/ButtonCreate.vue";
 
 export default {
-    name: 'Layout',
+    name: 'layout',
     components: {
         PageTitle,
+        ButtonCreate
     },
+    props: {
+        titleLayout: pageTitle
+    },
+    mounted() {
+        // this.
+    }
 };
 </script>
 
