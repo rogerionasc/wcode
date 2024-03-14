a323f24545a263d7ccb73c7ff1c09a0b32fc0312
Rogerio Nascimento <rogerionascimentosantos@gmail.com>
2024-03-13 23:46:40 -0300

    Layout create user

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -379,108 +379,128 @@
             <div class="modal-dialog modal-lg" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title">New report</h5>
+                        <h5 class="modal-title">Novo usuário</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <div class="mb-3">
-                            <label class="form-label">Name</label>
-                            <input type="text" class="form-control" name="example-text-input"
-                                   placeholder="Your report name">
-                        </div>
-                        <label class="form-label">Report type</label>
-                        <div class="form-selectgroup-boxes row mb-3">
+                        <div class="row">
                             <div class="col-lg-6">
-                                <label class="form-selectgroup-item">
-                                    <input type="radio" name="report-type" value="1" class="form-selectgroup-input"
-                                           checked>
-                                    <span class="form-selectgroup-label d-flex align-items-center p-3">
-                    <span class="me-3">
-                      <span class="form-selectgroup-check"></span>
-                    </span>
-                    <span class="form-selectgroup-label-content">
-                      <span class="form-selectgroup-title strong mb-1">Simple</span>
-                      <span class="d-block text-muted">Provide only basic data needed for the report</span>
-                    </span>
-                  </span>
-                                </label>
+                                <div class="mb-3">
+                                    <label class="form-label">Nome</label>
+                                    <input type="text" class="form-control" name="example-text-input" placeholder="Seu primeiro nome">
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6">
+                                <div class="mb-3">
+                                    <label class="form-label">Sobrenome</label>
+                                    <input type="text" class="form-control" name="example-text-input"
+                                           placeholder="Seu sobrenome">
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6">
+                                <div class="mb-3">
+                                    <label class="form-label">CPF</label>
+                                    <input type="text" class="form-control" name="example-text-input"
+                                           placeholder="00.000.000-00">
+                                </div>
                             </div>
                             <div class="col-lg-6">
-                                <label class="form-selectgroup-item">
-                                    <input type="radio" name="report-type" value="1" class="form-selectgroup-input">
-                                    <span class="form-selectgroup-label d-flex align-items-center p-3">
-                    <span class="me-3">
-                      <span class="form-selectgroup-check"></span>
-                    </span>
-                    <span class="form-selectgroup-label-content">
-                      <span class="form-selectgroup-title strong mb-1">Advanced</span>
-                      <span class="d-block text-muted">Insert charts and additional advanced analyses to be inserted in the report</span>
-                    </span>
-                  </span>
-                                </label>
+                                <div class="mb-3">
+                                    <label class="form-label">Data de nascimento</label>
+                                    <input type="date" class="form-control">
+                                </div>
                             </div>
                         </div>
+                    </div>
+                    <div class="modal-header">
+                        <h5 class="modal-title">Informação da conta</h5>
+                    </div>
+                    <div class="modal-body">
                         <div class="row">
                             <div class="col-lg-8">
                                 <div class="mb-3">
-                                    <label class="form-label">Report url</label>
-                                    <div class="input-group input-group-flat">
-                    <span class="input-group-text">
-                      https://tabler.io/reports/
-                    </span>
-                                        <input type="text" class="form-control ps-0" value="report-01"
-                                               autocomplete="off">
-                                    </div>
+                                    <label class="form-label">Email</label>
+                                    <input type="text" class="form-control" name="example-text-input"
+                                           placeholder="example@email.com">
                                 </div>
                             </div>
                             <div class="col-lg-4">
                                 <div class="mb-3">
-                                    <label class="form-label">Visibility</label>
+                                    <label class="form-label">Situação</label>
                                     <select class="form-select">
-                                        <option value="1" selected>Private</option>
-                                        <option value="2">Public</option>
-                                        <option value="3">Hidden</option>
+                                        <option value="ativo" selected>Ativo</option>
+                                        <option value="inativo">Inativo</option>
                                     </select>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <div class="modal-body">
-                        <div class="row">
+                            <div class="col-lg-6 ">
+                                <div class="mb-3">
+                                    <label class="form-label">Senha</label>
+                                    <input type="text" class="form-control" name="example-text-input"
+                                           placeholder="Digite sua senha">
+                                </div>
+                            </div>
+
                             <div class="col-lg-6">
                                 <div class="mb-3">
-                                    <label class="form-label">Client name</label>
-                                    <input type="text" class="form-control">
+                                    <label class="form-label">Confirme sua senha</label>
+                                    <input type="text" class="form-control" name="example-text-input"
+                                           placeholder="Confirme sua senha">
                                 </div>
+                            </div>
+
+                        </div>
+                        <label class="form-label">Tipo de conta</label>
+                        <div class="form-selectgroup-boxes row mb-3">
+                            <div class="col-lg-6">
+                                <label class="form-selectgroup-item h-100">
+                                    <input type="radio" name="report-type" value="1" class="form-selectgroup-input"
+                                           checked>
+                                    <span class="form-selectgroup-label d-flex align-items-center p-3">
+                                        <span class="me-3">
+                                            <span class="form-selectgroup-check">
+                                            </span>
+                                        </span>
+                                        <span class="form-selectgroup-label-content">
+                                            <span class="form-selectgroup-title strong mb-1">Padrão</span>
+                                            <span class="d-block text-muted">
+                                                Conta Padrão realiza tarefas básicas como ler, gravar e editar
+                                                informações, de forma limitada.
+
+                                            </span>
+                                        </span>
+                                    </span>
+                                </label>
                             </div>
                             <div class="col-lg-6">
-                                <div class="mb-3">
-                                    <label class="form-label">Reporting period</label>
-                                    <input type="date" class="form-control">
-                                </div>
-                            </div>
-                            <div class="col-lg-12">
-                                <div>
-                                    <label class="form-label">Additional information</label>
-                                    <textarea class="form-control" rows="3"></textarea>
-                                </div>
+                                <label class="form-selectgroup-item h-100">
+                                    <input type="radio" name="report-type" value="1" class="form-selectgroup-input">
+                                    <span class="form-selectgroup-label d-flex align-items-center p-3">
+                                        <span class="me-3">
+                                            <span class="form-selectgroup-check"></span>
+                                        </span>
+                                        <span class="form-selectgroup-label-content">
+                                            <span class="form-selectgroup-title strong mb-1">Administrador</span>
+                                            <span class="d-block text-muted">
+                                                Conta administradora possui privilégios especiais, responsável por
+                                                gerenciar e configurar o sistema.
+                                            </span>
+                                        </span>
+                                    </span>
+                                </label>
                             </div>
                         </div>
                     </div>
+
                     <div class="modal-footer">
                         <a href="#" class="btn btn-link link-secondary" data-bs-dismiss="modal">
-                            Cancel
+                            Cancelar
                         </a>
                         <a href="#" class="btn btn-primary ms-auto" data-bs-dismiss="modal">
                             <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
-                                 viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                 stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                <path d="M12 5l0 14"/>
-                                <path d="M5 12l14 0"/>
-                            </svg>
-                            Create new report
+                            Salvar
                         </a>
                     </div>
                 </div>
